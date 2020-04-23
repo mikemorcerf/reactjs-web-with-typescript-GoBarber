7155c475a15a1d8024e56e646bbc66ba9feb74fb
Michael Moura <mikemorcerf@gmail.com>
2020-04-23 18:01:33 -0400

    implement validation to SignUp form

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,9 +11,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-	function handleSubmit(data: object): void {
-		console.log(data);
-	}
+	const handleSubmit = useCallback(async (data: object) => {
+		try {
+			const schema = Yup.object().shape({
+				name: Yup.string().required('Name is required'),
+				email: Yup.string()
+					.required('Email is required')
+					.email('Type a valid email'),
+				password: Yup.string().min(6, 'Password must have at least 6 digits'),
+			});
+
+			await schema.validate(data, {
+				abortEarly: false,
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	}, []);
 
 	return (
 		<Container>
