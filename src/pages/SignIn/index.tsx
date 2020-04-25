07b5774acc3966241b1c492363d96df373b3c32a
Michael Moura <mikemorcerf@gmail.com>
2020-04-24 20:12:17 -0400

    create signOut in AuthContext

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,7 +9,7 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
