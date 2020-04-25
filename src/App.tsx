a2649dcd7346f2dcda3a1046d64b1d62f0fc1024
Michael Moura <mikemorcerf@gmail.com>
2020-04-25 15:17:28 -0400

    implement Toast message component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
@@ -12,6 +13,7 @@ const App: React.FC = () => (
 			<SignIn />
 		</AuthProvider>
 
+		<ToastContainer />
 		<GlobalStyle />
 	</>
 );
