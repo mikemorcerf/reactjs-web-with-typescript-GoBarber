82f2fa0cf6d6e6340c67cb0e7bc478f918f987e4
Michael Moura <mikemorcerf@gmail.com>
2020-05-23 16:31:25 -0400

    create ForgotPassword page

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -10,26 +10,23 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background, AnimationContainer } from './styles';
 
-interface SignInFormData {
+interface ForgotPasswordFormData {
 	email: string;
-	password: string;
 }
 
-const SignIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
 	const formRef = useRef<FormHandles>(null);
-	const history = useHistory();
+	// const history = useHistory();
 
-	const { signIn } = useAuth();
 	const { addToast } = useToast();
 
 	const handleSubmit = useCallback(
-		async (data: SignInFormData) => {
+		async (data: ForgotPasswordFormData) => {
 			try {
 				formRef.current?.setErrors({});
 
@@ -37,19 +34,16 @@ const SignIn: React.FC = () => {
 					email: Yup.string()
 						.required('Email is required')
 						.email('Type a valid email'),
-					password: Yup.string().required('Password is required'),
 				});
 
 				await schema.validate(data, {
 					abortEarly: false,
 				});
 
-				await signIn({
-					email: data.email,
-					password: data.password,
-				});
+				// TODO
+				// Recover password
 
-				history.push('/dashboard');
+				// history.push('/dashboard');
 			} catch (err) {
 				if (err instanceof Yup.ValidationError) {
 					const errors = getValidationErrors(err);
@@ -59,13 +53,13 @@ const SignIn: React.FC = () => {
 
 				addToast({
 					type: 'error',
-					title: 'Authentication Error',
+					title: 'Password recovery Error',
 					description:
-						'An error ocurred when trying to signin. Check your credentials',
+						'An error ocurred when trying to recover password. Try again',
 				});
 			}
 		},
-		[signIn, addToast, history],
+		[addToast],
 	);
 
 	return (
@@ -74,24 +68,18 @@ const SignIn: React.FC = () => {
 				<AnimationContainer>
 					<img src={logoImg} alt="GoBarber" />
 					<Form ref={formRef} onSubmit={handleSubmit}>
-						<h1>Sign in</h1>
+						<h1>Password recovery</h1>
 						<Input
 							name="email"
 							icon={FiMail}
 							placeholder="Your email address"
 						/>
-						<Input
-							name="password"
-							icon={FiLock}
-							type="password"
-							placeholder="password"
-						/>
-						<Button type="submit">Enter</Button>
-						<Link to="/forgot-password">Forgot password?</Link>
+
+						<Button type="submit">Submit</Button>
 					</Form>
-					<Link to="/signup">
-						<FiLogIn />
-						Sign up
+					<Link to="/">
+						<FiArrowLeft />
+						Sign in
 					</Link>
 				</AnimationContainer>
 			</Content>
@@ -100,4 +88,4 @@ const SignIn: React.FC = () => {
 	);
 };
 
-export default SignIn;
+export default ForgotPassword;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
+import Route from './Route';
+
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
-import Route from './Route';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -12,6 +13,7 @@ const Routes: React.FC = () => (
 	<Switch>
 		<Route path="/" exact component={SignIn} />
 		<Route path="/signup" component={SignUp} />
+		<Route path="/forgot-password" component={ForgotPassword} />
 
 		<Route path="/dashboard" component={Dashboard} isPrivate />
 	</Switch>
