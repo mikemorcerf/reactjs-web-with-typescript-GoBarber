7a15b3098be2e8e4dbcbd9a61314bd00f51f9fb7
Michael Moura <mikemorcerf@gmail.com>
2020-05-25 19:16:04 -0400

    implement Next Appointment section on Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+	Container,
+	Header,
+	HeaderContent,
+	Profile,
+	Content,
+	Schedule,
+	NextAppointment,
+	Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
 					</button>
 				</HeaderContent>
 			</Header>
+
+			<Content>
+				<Schedule>
+					<h1>Appointments</h1>
+					<p>
+						<span>Today</span>
+						<span>May 6th</span>
+						<span>Monday</span>
+					</p>
+
+					<NextAppointment>
+						<strong>Next appointment</strong>
+						<div>
+							<img
+								src="https://avatars3.githubusercontent.com/u/16601136?s=460&u=cc113eafff21220b4a2ef9c77d0456b6ff6175ce&v=4"
+								alt="Mike Morcerf"
+							/>
+
+							<strong>Mike Morcerf</strong>
+							<span>
+								<FiClock />
+								08:00
+							</span>
+						</div>
+					</NextAppointment>
+				</Schedule>
+				<Calendar />
+			</Content>
 		</Container>
 	);
 };
